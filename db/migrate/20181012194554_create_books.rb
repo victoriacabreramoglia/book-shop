02c169fec3541d7fe384d580cc7d3a34ab021d96
victorioc <victoriocabrera@gmail.com>
2018-10-12 15:53:21 -0400

    initial commit. model basics. no associations.

---
+++ db/migrate/20181012194554_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.text :body
+      t.integer :author_id
+
+      t.timestamps
+    end
+  end
+end
